440b77d6e8331283390fb64bbb899e5a8e0cda27
adrianC56 <adriancrespog3@gmail.com>
2021-12-13 18:20:31 -0400

    arrg2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const data = require('./assets/data');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;;
 const URL= 'http://localhost';
 
 app.use(cors({
